99e851cf6a5da656d01f970ead1c5dde3af528ab
Kevin McNalley <kevin.c.mcnalley@gmail.com>
2018-10-26 14:39:04 -0700

    Update twoDArray.js

--- twoDArray.js
+++ twoDArray.js
@@ -1,31 +1,3 @@
-/*Complete the function hourglassSum in the editor below. It should return an integer, the maximum hourglass sum in the array.*/
-
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 // Complete the hourglassSum function below.
 function hourglassSum(arr) {
     let max = -999999999;
@@ -41,13 +13,11 @@ function hourglassSum(arr) {
     return max
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-    let arr = Array(6);
-    for (let i = 0; i < 6; i++) {
-        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
-    }
-    let result = hourglassSum(arr);
-    ws.write(result + "\n");
-    ws.end();
-}
+let test = [[1,2,3,4,5,6],
+            [7,8,9,1,2,3],
+            [4,5,6,7,8,9],
+            [1,2,3,4,5,6],
+            [7,8,9,1,2,3],
+            [4,5,6,7,8,9]]
+
+console.log(hourglassSum(test));
